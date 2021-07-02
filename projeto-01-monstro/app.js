faf4fff8cf954a07be4770c168a942e514ebe9db
Fabio Antunes <fabio.avasc@gmail.com>
2021-07-02 18:01:43 -0300

    Conclusão do Projeto #1

---
+++ projeto-01-monstro/app.js
@@ -0,0 +1,60 @@
+new Vue({
+    el: '#app',
+    data: {
+        playerLife: 100,
+        monsterLife: 100,
+        playing: false,
+        logs: []
+
+    },
+    computed: {
+        hasResult(){
+            return this.playerLife == 0 | this.monsterLife == 0
+        }, 
+
+    },
+    methods: {
+        start(){
+            this.playing = true 
+            this.playerLife = 100
+            this.monsterLife = 100
+            this.logs = []
+        }, 
+        attack(especial){
+            this.hurt('monsterLife', 5, 10, especial, 'Jogador', 'Monstro', 'player')
+            if(this.monsterLife > 0){
+                this.hurt('playerLife', 7, 12, false, 'Monstro', 'Jogador', 'monster')
+            }
+            console.log(this.playerLife)
+            console.log(this.monsterLife)
+        },
+        hurt(persona, min, max, especial, source, target, cls){
+            const plus = especial ?  5 : 0
+            const hurt = this.getRandom (min + plus, max + plus)
+            this[persona] = Math.max(this[persona] - hurt, 0)
+            this.registerLog(`${source} atingiu o ${target} com ${hurt}.`, cls)
+        },
+        heal(min, max){
+            const heal = this.getRandom(min, max)
+            this.playerLife = Math.min(this.playerLife + heal, 100)
+            this.registerLog(`Jogador se curou e recuperou o HP em ${heal}.`, 'player');
+        },
+        healAndHurt(){
+            this.heal(10,15)
+            this.hurt('playerLife', 7, 12, false, 'Monstro', 'Jogador', 'monster')
+        },
+        getRandom(min, max){
+            const value = Math.random() * (max-min) + min;
+            return Math.round(value)
+        },
+        registerLog(text, cls){
+            this.logs.unshift({ text, cls })
+        }
+
+    },
+    watch: {
+        hasResult(value){
+            if (value) this.playing = false
+        }
+    }
+})
